dc343a5cde86aca308ff66fc3253618aa3df710a
Loewe_111 <m.loewe_111@gmail.com>
2023-04-18 20:57:21 +0200

    using argument tables

--- demo.lua
+++ demo.lua
@@ -1,27 +1,26 @@
-lib = require("cctinker")
-screen = lib:new(term)
+local lib = require("cctinker")
+local screen = lib:new(term)
 
-lorem = "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate repellendus omnis, adipisci deleniti quo eos explicabo nesciunt!"
+local lorem = "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate repellendus omnis, adipisci deleniti quo eos explicabo nesciunt!"
 
-screen:text(10, 2, "ccTinker Demo by Loewe_111", colors.lime, colors.black)
+screen:text({x=2, y=2, text = "ccTinker demo", color = colors.lime})
 
-screen:button(2, 4, "Change the color!", colors.black, colors.blue, function()
+screen:button({x=2, y=4, text="Change the color!", color=colors.black, background=colors.blue, callback=function()
   screen:setBackground(screen.COLORS[math.random(1, #screen.COLORS)])
-end)
+end})
 
-screen:button(2, 2, "EXIT", colors.red, colors.black, function()
-  screen:clear()
-  screen:stopLoop()
-end)
+screen:button({x=screen.X-5, y=screen.Y-1, text="Exit", color=colors.white, background=colors.red, callback=function()
+  screen:exit()
+end})
 
-screen:textarea(2, 6, 45, 5, "cctinker is a tkinter-like Computercraft Graphics API by Loewe_111. You are reading this inside of a Textarea, which wraps its text.", colors.black, colors.white)
+screen:textarea({x=2, y=6, width=45, height=5, text="cctinker is a tkinter-like Computercraft Graphics API by Loewe_111. You are reading this inside of a Textarea, which wraps its text.", color=colors.black, background=colors.white})
 
-screen:checkbox(2, 12, "Enable Cool Lorem Ipsum", colors.black, colors.white, function(checked)
+screen:checkbox({x=2, y=12, text="Enable Cool Lorem Ipsum", color=colors.black, background=colors.white, callback=function(_, _, _, checked)
   if checked then
-    screen:textarea(2, 14, 40, 4, lorem, colors.lime, colors.black, "lorem")
+    screen:textarea({x=2, y=14, width=40, height=4, text=lorem, color=colors.lime, background=colors.black, id="lorem"})
   else
     screen:remove("lorem")
   end
-end)
+end})
 
 screen:loop()
