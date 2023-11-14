5646719140c423b717ef0e934e34c24a5c73f269
Loewe111 <m.loewe111@gmail.com>
2023-11-14 19:57:33 +0100

    add double buffering

--- cctinker.lua
+++ cctinker.lua
@@ -6,12 +6,24 @@ function cctinker:new(termObject)
   local o = {}
   setmetatable(o, self)
   self.__index = self
-  o.term = termObject or term
+  o.term = termObject or term.current()
   o.X, o.Y = o.term.getSize()
   o.background = colors.black
   o.COLORS = {colors.white, colors.orange, colors.magenta, colors.lightBlue, colors.yellow, colors.lime, colors.pink, colors.gray, colors.lightGray, colors.cyan, colors.purple, colors.blue, colors.brown, colors.green, colors.red, colors.black}
   o.children = {}
   o.eventListeners = {}
+  o.buffers = {}
+  o.screenState = {
+    cursorBlink = false,
+    blinkX = 1, 
+    blinkY = 1,
+    cursorX = 1,
+    cursorY = 1,
+    color = "0",
+    background = "f",
+    currentBuffer = 1,
+    drawBuffer = 2
+  }
   return o
 end
 
@@ -32,18 +44,55 @@ function cctinker:_checkArgs(args, requiredArgs, optionalArgs)
   end
 end
 
-function cctinker:_draw()
-  self.term.setBackgroundColor(self.background)
+function cctinker:_newBuffer()
+  local buffer = {}
+  for y=1, self.Y do
+    buffer[y] = {}
+    buffer[y].text = string.rep(" ", self.X)
+    buffer[y].fg = string.rep("0", self.X)
+    buffer[y].bg = string.rep(colors.toBlit(self.background), self.X)
+  end 
+  return buffer
+end
+
+function cctinker:_drawBuffer(buffer_id)
+  self.term.setCursorBlink(false)
   self.term.clear()
+  local buffer = self.buffers[buffer_id]
+  for y=1, #buffer do
+    self.term.setCursorPos(1, y)
+    self.term.blit(buffer[y].text, buffer[y].fg, buffer[y].bg)
+  end
+  self.term.setCursorPos(self.screenState.blinkX, self.screenState.blinkY)
+  self.term.setCursorBlink(self.screenState.cursorBlink)
+end
+
+function cctinker:_compareBuffers(buffer_id_a, buffer_id_b) 
+  local bufferA = self.buffers[buffer_id_a]
+  local bufferB = self.buffers[buffer_id_b]
+  -- Compare buffers and return false if they are different
+  for y=1, #bufferA do
+    if bufferA[y].text ~= bufferB[y].text or bufferA[y].fg ~= bufferB[y].fg or bufferA[y].bg ~= bufferB[y].bg then
+      return false
+    end
+  end
+  return true
+end
+
+function cctinker:_draw()
+  self.buffers[self.screenState.drawBuffer] = self:_newBuffer()
   for i, v in pairs(self.children) do
     if v.visible == true then
       v:draw()
     end
   end
+  if not self:_compareBuffers(self.screenState.currentBuffer, self.screenState.drawBuffer) then
+    self.screenState.drawBuffer, self.screenState.currentBuffer = self.screenState.currentBuffer, self.screenState.drawBuffer
+    self:_drawBuffer(self.screenState.currentBuffer)
+  end
 end
 
 function cctinker:_handleEvent(event, eventData)
-
   if event == "mouse_click" then
     local button, x, y = eventData[2], eventData[3], eventData[4]
     for i, obj in pairs(self.children) do
@@ -155,6 +204,7 @@ function cctinker:loop()
     end
   end
   self.looping = true
+  self.buffers = {self:_newBuffer(), self:_newBuffer()}
   parallel.waitForAny(eventLoop, drawLoop)
 end
 
@@ -182,6 +232,66 @@ function cctinker:exit()
   self.term.setCursorPos(1, 1)
 end
 
+-- Drawing functions
+
+function cctinker:setCursorPos(x, y)
+  self.screenState.cursorX = x
+  self.screenState.cursorY = y
+end
+
+function cctinker:getCursorPos()
+  return self.screenState.cursorX, self.screenState.cursorY
+end
+
+function cctinker:setTextColor(color)
+  self.screenState.color = colors.toBlit(color)
+end
+
+function cctinker:getTextColor()
+  return colors.fromBlit(self.screenState.color)
+end
+
+function cctinker:setBackgroundColor(color)
+  self.screenState.background = colors.toBlit(color)
+end
+
+function cctinker:getBackgroundColor()
+  return colors.fromBlit(self.screenState.background)
+end
+
+function cctinker:setCursorBlink(blink, x, y)
+  self.screenState.cursorBlink = blink
+  self.screenState.blinkX = x or self.screenState.cursorX
+  self.screenState.blinkY = y or self.screenState.cursorY
+
+  self.term.setCursorBlink(blink)
+  self.term.setCursorPos(self.screenState.blinkX, self.screenState.blinkY)
+end
+
+function cctinker:write(text)
+  local buffer = self.buffers[self.screenState.drawBuffer]
+  buffer[self.screenState.cursorY].text = replace_text(buffer[self.screenState.cursorY].text, text, self.screenState.cursorX, self.screenState.cursorX + #text - 1)
+  buffer[self.screenState.cursorY].fg = replace_text(buffer[self.screenState.cursorY].fg, string.rep(self.screenState.color, #text), self.screenState.cursorX, self.screenState.cursorX + #text - 1)
+  buffer[self.screenState.cursorY].bg = replace_text(buffer[self.screenState.cursorY].bg, string.rep(self.screenState.background, #text), self.screenState.cursorX, self.screenState.cursorX + #text - 1)
+  self.screenState.cursorX = self.screenState.cursorX + #text
+  self.buffers[self.screenState.drawBuffer] = buffer
+end
+
+function cctinker:blit(text, fg, bg) 
+  local buffer = self.buffers[self.screenState.drawBuffer]
+  buffer[self.screenState.cursorY].text = replace_text(buffer[self.screenState.cursorY].text, text, self.screenState.cursorX, self.screenState.cursorX + #text - 1)
+  buffer[self.screenState.cursorY].fg = replace_text(buffer[self.screenState.cursorY].fg, fg, self.screenState.cursorX, self.screenState.cursorX + #text - 1)
+  buffer[self.screenState.cursorY].bg = replace_text(buffer[self.screenState.cursorY].bg, bg, self.screenState.cursorX, self.screenState.cursorX + #text - 1)
+  self.screenState.cursorX = self.screenState.cursorX + #text
+  self.buffers[self.screenState.drawBuffer] = buffer
+end
+
+function replace_text(str, replaceStr, pos, length)
+  return str:sub(1, pos-1) .. replaceStr .. str:sub(length+1)
+end
+
+-- Widget functions
+
 function cctinker:getObject(id)
   return self.children[id]
 end
@@ -205,6 +315,8 @@ end
 function cctinker:setObjects(objects)
   self.children = objects
 end
+
+-- Widgets
 
 function cctinker:frame(args)
   local requiredArgs = {x="number", y="number", width="number", height="number"}
@@ -241,10 +353,10 @@ function cctinker:text(args)
     visible = args.visible == nil or args.visible == true
   }
   textObject.draw = function()
-    self.term.setCursorPos(textObject.x, textObject.y)
-    self.term.setTextColor(textObject.color)
-    self.term.setBackgroundColor(textObject.background)
-    self.term.write(textObject.text)
+    self:setCursorPos(textObject.x, textObject.y)
+    self:setTextColor(textObject.color)
+    self:setBackgroundColor(textObject.background)
+    self:write(textObject.text)
   end
   self.children[textObject.id] = textObject
   return textObject
@@ -267,14 +379,14 @@ function cctinker:textarea(args)
     visible = args.visible == nil or args.visible == true
   }
   textareaObject.draw = function()
-    self.term.setTextColor(textareaObject.color)
-    self.term.setBackgroundColor(textareaObject.background)
+    self:setTextColor(textareaObject.color)
+    self:setBackgroundColor(textareaObject.background)
     local ccstrings = require("cc.strings")
     local texts = ccstrings.wrap(textareaObject.text, textareaObject.width)
     for i = 1, textareaObject.height do
       local text = ccstrings.ensure_width(texts[i] or "", textareaObject.width)
-      self.term.setCursorPos(textareaObject.x, textareaObject.y + i - 1)
-      self.term.write(text)
+      self:setCursorPos(textareaObject.x, textareaObject.y + i - 1)
+      self:write(text)
     end
   end
   self.children[textareaObject.id] = textareaObject
@@ -299,10 +411,10 @@ function cctinker:button(args)
     visible = args.visible == nil or args.visible == true
   }
   buttonObject.draw = function()
-    self.term.setCursorPos(buttonObject.x, buttonObject.y)
-    self.term.setTextColor(buttonObject.color)
-    self.term.setBackgroundColor(buttonObject.background)
-    self.term.write(buttonObject.text)
+    self:setCursorPos(buttonObject.x, buttonObject.y)
+    self:setTextColor(buttonObject.color)
+    self:setBackgroundColor(buttonObject.background)
+    self:write(buttonObject.text)
   end
   self.children[buttonObject.id] = buttonObject
   return buttonObject
@@ -327,16 +439,16 @@ function cctinker:checkbox(args)
     visible = args.visible == nil or args.visible == true
   }
   checkboxObject.draw = function()
-    self.term.setCursorPos(checkboxObject.x, checkboxObject.y)
-    self.term.setTextColor(checkboxObject.color)
-    self.term.setBackgroundColor(checkboxObject.background)
-    self.term.write("[")
+    self:setCursorPos(checkboxObject.x, checkboxObject.y)
+    self:setTextColor(checkboxObject.color)
+    self:setBackgroundColor(checkboxObject.background)
+    self:write("[")
     if checkboxObject.checked then
-      self.term.write("x")
+      self:write("x")
     else
-      self.term.write(" ")
+      self:write(" ")
     end
-    self.term.write("] " .. checkboxObject.text)
+    self:write("] " .. checkboxObject.text)
   end
   checkboxObject.event_click = function(x, y, button)
     checkboxObject.checked = not checkboxObject.checked
@@ -371,16 +483,16 @@ function cctinker:radio(args)
   end
   radioObject.draw = function()
     for i = 1, #radioObject.options do
-      self.term.setCursorPos(radioObject.x, radioObject.y + i - 1)
-      self.term.setTextColor(radioObject.color)
-      self.term.setBackgroundColor(radioObject.background)
-      self.term.write("(")
+      self:setCursorPos(radioObject.x, radioObject.y + i - 1)
+      self:setTextColor(radioObject.color)
+      self:setBackgroundColor(radioObject.background)
+      self:write("(")
       if radioObject.selected == i then
-        self.term.write("\007")
+        self:write("\007")
       else
-        self.term.write(" ")
+        self:write(" ")
       end
-      self.term.write(") " .. radioObject.options[i])
+      self:write(") " .. radioObject.options[i])
     end
   end
 
@@ -422,18 +534,18 @@ function cctinker:switch(args)
     end
   end
   switchObject.draw = function()
-    self.term.setCursorPos(switchObject.x, switchObject.y)
-    self.term.setTextColor(colors.white)
+    self:setCursorPos(switchObject.x, switchObject.y)
+    self:setTextColor(colors.white)
     if switchObject.state then
-      self.term.setBackgroundColor(colors.lime)
-      self.term.write("  \127")
+      self:setBackgroundColor(colors.lime)
+      self:write("  \127")
     else
-      self.term.setBackgroundColor(colors.red)
-      self.term.write("\127  ")
+      self:setBackgroundColor(colors.red)
+      self:write("\127  ")
     end
-    self.term.setTextColor(switchObject.color)
-    self.term.setBackgroundColor(switchObject.background)
-    self.term.write(" " .. switchObject.text)
+    self:setTextColor(switchObject.color)
+    self:setBackgroundColor(switchObject.background)
+    self:write(" " .. switchObject.text)
   end
   
   self.children[switchObject.id] = switchObject 
@@ -466,14 +578,14 @@ function cctinker:tabSwitch(args)
   tabSwitchObject.draw = function()
     local x = tabSwitchObject.x
     for i = 1, #tabSwitchObject.tabs do
-      self.term.setCursorPos(x, tabSwitchObject.y)
-      self.term.setTextColor(tabSwitchObject.color)
-      self.term.setBackgroundColor(tabSwitchObject.background)
+      self:setCursorPos(x, tabSwitchObject.y)
+      self:setTextColor(tabSwitchObject.color)
+      self:setBackgroundColor(tabSwitchObject.background)
       if tabSwitchObject.selected == i then
-        self.term.setTextColor(tabSwitchObject.selectedColor)
-        self.term.setBackgroundColor(tabSwitchObject.selectedBackground)
+        self:setTextColor(tabSwitchObject.selectedColor)
+        self:setBackgroundColor(tabSwitchObject.selectedBackground)
       end
-      self.term.write("\149" .. tabSwitchObject.tabs[i] .. " ")
+      self:write("\149" .. tabSwitchObject.tabs[i] .. " ")
       x = x + #tabSwitchObject.tabs[i] + 2
     end
   end
@@ -525,22 +637,22 @@ function cctinker:input(args)
   }
   inputObject.draw = function()
     local ccstrings = require("cc.strings")
-    self.term.setCursorPos(inputObject.x, inputObject.y)
+    self:setCursorPos(inputObject.x, inputObject.y)
     if inputObject.input.text == "" then
-      self.term.setTextColor(inputObject.placeholderColor)
-      self.term.setBackgroundColor(inputObject.background)
-      self.term.write(ccstrings.ensure_width(string.sub(inputObject.placeholder, -inputObject.width), inputObject.width))
+      self:setTextColor(inputObject.placeholderColor)
+      self:setBackgroundColor(inputObject.background)
+      self:write(ccstrings.ensure_width(string.sub(inputObject.placeholder, -inputObject.width), inputObject.width))
     else
-      self.term.setTextColor(inputObject.color)
-      self.term.setBackgroundColor(inputObject.background)
-      self.term.write(ccstrings.ensure_width(string.sub(inputObject.input.text, -inputObject.width), inputObject.width))
+      self:setTextColor(inputObject.color)
+      self:setBackgroundColor(inputObject.background)
+      self:write(ccstrings.ensure_width(string.sub(inputObject.input.text, -inputObject.width), inputObject.width))
     end
     if inputObject.input.cursor > inputObject.width then
-      self.term.setCursorPos(inputObject.x + inputObject.width, inputObject.y)
+      self:setCursorPos(inputObject.x + inputObject.width, inputObject.y)
     else
-      self.term.setCursorPos(inputObject.x + inputObject.input.cursor - 1, inputObject.y)
+      self:setCursorPos(inputObject.x + inputObject.input.cursor - 1, inputObject.y)
     end
-    self.term.setCursorBlink(inputObject.input.focused)
+    self:setCursorBlink(inputObject.input.focused, inputObject.x + inputObject.input.cursor - 1, inputObject.y)
   end
   inputObject.event_click = function(x, y, button)
     inputObject.input.focused = true
@@ -622,32 +734,32 @@ function cctinker:inputArea(args)
   }
   inputAreaObject.draw = function()
     local ccstrings = require("cc.strings")
-    self.term.setCursorPos(inputAreaObject.x, inputAreaObject.y)
+    self:setCursorPos(inputAreaObject.x, inputAreaObject.y)
     if inputAreaObject.input.text == "" then
-      self.term.setTextColor(inputAreaObject.placeholderColor)
-      self.term.setBackgroundColor(inputAreaObject.background)
+      self:setTextColor(inputAreaObject.placeholderColor)
+      self:setBackgroundColor(inputAreaObject.background)
       local placeholder = ccstrings.wrap(inputAreaObject.placeholder, inputAreaObject.width)
       for i = 1, inputAreaObject.height do
         local text = ccstrings.ensure_width(placeholder[i] or "", inputAreaObject.width)
-        self.term.setCursorPos(inputAreaObject.x, inputAreaObject.y + i - 1)
-        self.term.write(text)
+        self:setCursorPos(inputAreaObject.x, inputAreaObject.y + i - 1)
+        self:write(text)
       end
     else
-      self.term.setTextColor(inputAreaObject.color)
-      self.term.setBackgroundColor(inputAreaObject.background)
+      self:setTextColor(inputAreaObject.color)
+      self:setBackgroundColor(inputAreaObject.background)
       local texts = ccstrings.wrap(inputAreaObject.input.text, inputAreaObject.width)
       for i = 1, inputAreaObject.height do
         local text = ccstrings.ensure_width(texts[i] or "", inputAreaObject.width)
-        self.term.setCursorPos(inputAreaObject.x, inputAreaObject.y + i - 1)
-        self.term.write(text)
+        self:setCursorPos(inputAreaObject.x, inputAreaObject.y + i - 1)
+        self:write(text)
       end
     end
     if inputAreaObject.input.cursor > inputAreaObject.width then
-      self.term.setCursorPos(inputAreaObject.x + inputAreaObject.width, inputAreaObject.y)
+      self:setCursorPos(inputAreaObject.x + inputAreaObject.width, inputAreaObject.y)
     else
-      self.term.setCursorPos(inputAreaObject.x + inputAreaObject.input.cursor - 1, inputAreaObject.y)
+      self:setCursorPos(inputAreaObject.x + inputAreaObject.input.cursor - 1, inputAreaObject.y)
     end
-    self.term.setCursorBlink(inputAreaObject.input.focused)
+    -- self:setCursorBlink(inputAreaObject.input.focused)
   end
   inputAreaObject.event_click = function(x, y, button)
     inputAreaObject.input.focused = true
